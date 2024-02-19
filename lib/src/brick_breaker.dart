f89d2c03493eabb74c58bdce45a230341f0eb291
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-19 15:54:51 +0530

    added ball, bat and collision

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';                             // Add this import
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';                         // And this import
+import 'package:flutter/services.dart';                         // And this
 
 import 'components/components.dart';
 import 'config.dart';
 
-class BrickBreaker extends FlameGame {
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents {                // Modify this line
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -15,6 +20,7 @@ class BrickBreaker extends FlameGame {
           ),
         );
 
+  final rand = math.Random();
   double get width => size.x;
   double get height => size.y;
 
@@ -25,5 +31,32 @@ class BrickBreaker extends FlameGame {
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(PlayArea());
+
+    world.add(Ball(
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
+            .normalized()
+          ..scale(height / 4)));
+
+    world.add(Bat(                                              // Add from here...
+        size: Vector2(batWidth, batHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95)));          // To here
+
+    debugMode = true;
   }
+
+  @override                                                     // Add from here...
+  KeyEventResult onKeyEvent(
+      RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+    }
+    return KeyEventResult.handled;
+  }                                                             // To here
 }
