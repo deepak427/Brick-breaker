63a2e1061757448efce27587a808c3cfc35b3a4a
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-19 14:43:09 +0530

    created playarea

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'src/brick_breaker.dart';                                // Add this import
+
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  final game = BrickBreaker();                                  // Modify this line
+  runApp(GameWidget(game: game));
 }
